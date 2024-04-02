c8a0947ac3666b2c11fcd49438fcf6de51b6948b
chika <chika@omi>
2024-04-02 21:34:03 +0900

    ini

---
+++ useful_git/src/main/java/jp/iceleaf/useful/UsefulGitApplication.java
@@ -0,0 +1,13 @@
+package jp.iceleaf.useful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsefulGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsefulGitApplication.class, args);
+	}
+
+}
---
+++ useful_git/src/test/java/jp/iceleaf/useful/UsefulGitApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.iceleaf.useful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsefulGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
